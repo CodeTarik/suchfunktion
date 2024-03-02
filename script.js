6347ea82967e217b6ef3fab4e146a089284df2cf
Tarik Bozueslue <bozueslue_t@outlook.de>
2024-03-02 21:11:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let names = ['Alex', 'Chris', 'Daniel']
+
+function showNames() {
+    let list = document.getElementById('list');
+
+    list.innerHTML = '';
+
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        list.innerHTML += `<li>${name}</li>`;
+    }
+}
+
+
+function filterNames() {
+    let search = document.getElementById('search').value;
+    search = search.toLowerCase();
+    console.log(search);
+
+    let list = document.getElementById('list');
+
+    list.innerHTML = '';
+
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        if (name.toLowerCase().includes(search)) {
+            list.innerHTML += `<li>${name}</li>`;
+        }
+    }
+}
